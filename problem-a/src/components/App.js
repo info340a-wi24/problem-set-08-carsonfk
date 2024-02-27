b7a778fd9e810f5c065f82932bc055f34791a430
carsonfk <carson.f.k@outlook.com>
2024-02-26 23:08:40 -0800

    problem-a progress

--- problem-a/src/components/App.js
+++ problem-a/src/components/App.js
@@ -5,6 +5,8 @@ import PetList from './PetList';
 import AboutPage from './AboutPage';
 import ResourcesPage from './ResourcesPage';
 import PetDetail from './PetDetail';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 
 import SAMPLE_DOGS from '../data/dogs.json'; //a sample list of dogs (model)
 
@@ -13,29 +15,41 @@ function App(props) {
   const pets = SAMPLE_DOGS; //pretend this was loaded externally or via prop
 
   return (
-    <div>
-      <header className="jumbotron py-4 mb-4">
-        <div className="container">
-          <h1>Adopt a Pet</h1>
-        </div>
-      </header>
+    <Routes>
+        <Route path="about" element={<AboutPage />} />
+        <Route path="resources" element={<ResourcesPage />} />
+        <Route path="adopt" element={<PetPage />} />
+        <Route path="index" element={<PetList props={props.pets} />} />
+    </Routes>
     
-      <main className="container">
-        <div className="row">
-          <div className="col-3">
-            <AboutNav />
-          </div>
-          <div className="col">
-            <PetList pets={pets} />
-          </div>
-        </div>
-      </main>
-
-      <footer className="container">
-        <small>Images from <a href="http://www.seattlehumane.org/adoption/dogs">Seattle Humane Society</a></small>
-      </footer>
-    </div>
   );
 }
 
 export default App;
+
+/*
+
+<div>
+  <header className="jumbotron py-4 mb-4">
+    <div className="container">
+      <h1>Adopt a Pet</h1>
+    </div>
+  </header>
+    
+  <main className="container">
+    <div className="row">
+      <div className="col-3">
+        <AboutNav />
+      </div>
+      <div className="col">
+        <PetList pets={pets} />
+      </div>
+    </div>
+  </main>
+
+  <footer className="container">
+    <small>Images from <a href="http://www.seattlehumane.org/adoption/dogs">Seattle Humane Society</a></small>
+  </footer>
+</div>
+
+*/
