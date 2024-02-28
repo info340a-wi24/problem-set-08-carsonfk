e4f822d6643d0b43a6cc27c118b034df8db7bf00
carsonfk <carson.f.k@outlook.com>
2024-02-27 20:13:18 -0800

    finish problem-a

--- problem-a/src/components/App.js
+++ problem-a/src/components/App.js
@@ -5,8 +5,9 @@ import PetList from './PetList';
 import AboutPage from './AboutPage';
 import ResourcesPage from './ResourcesPage';
 import PetDetail from './PetDetail';
-import { Route } from 'react-router-dom';
+import { Navigate, Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import SAMPLE_DOGS from '../data/dogs.json'; //a sample list of dogs (model)
 
@@ -15,41 +16,37 @@ function App(props) {
   const pets = SAMPLE_DOGS; //pretend this was loaded externally or via prop
 
   return (
-    <Routes>
-        <Route path="about" element={<AboutPage />} />
-        <Route path="resources" element={<ResourcesPage />} />
-        <Route path="adopt" element={<PetPage />} />
-        <Route path="index" element={<PetList props={props.pets} />} />
-    </Routes>
-    
+    <div>
+      <header className="jumbotron py-4 mb-4">
+        <div className="container">
+        <h1><Link to="/adopt">Adopt a Pet</Link></h1>
+        </div>
+      </header>
+        
+      <main className="container">
+        <div className="row">
+          <div className="col-3">
+            <AboutNav />
+          </div>
+          <div className="col">
+            <Routes>
+              <Route path="about" element={<AboutPage />} />
+              <Route path="resources" element={<ResourcesPage />} />
+              <Route path="adopt" element={<PetPage />} >
+                <Route path=":petName" element={<PetDetail />} />
+                <Route index element={<PetList pets={pets} />} />
+              </Route>
+              <Route path="/*" element={<Navigate to="/adopt"/>} />
+            </Routes>
+          </div>
+        </div>
+      </main>
+
+      <footer className="container">
+        <small>Images from <a href="http://www.seattlehumane.org/adoption/dogs">Seattle Humane Society</a></small>
+      </footer>
+    </div>
   );
 }
 
 export default App;
-
-/*
-
-<div>
-  <header className="jumbotron py-4 mb-4">
-    <div className="container">
-      <h1>Adopt a Pet</h1>
-    </div>
-  </header>
-    
-  <main className="container">
-    <div className="row">
-      <div className="col-3">
-        <AboutNav />
-      </div>
-      <div className="col">
-        <PetList pets={pets} />
-      </div>
-    </div>
-  </main>
-
-  <footer className="container">
-    <small>Images from <a href="http://www.seattlehumane.org/adoption/dogs">Seattle Humane Society</a></small>
-  </footer>
-</div>
-
-*/
